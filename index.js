63bb8e73088dd2a1738257a5090572aed273e078
Christopher <chris.brower.96@gmail.com>
2022-12-17 00:27:01 -0700

    commit tree fa716263ceb5f947efff8643f27c9276409c8031

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {
+    name: "jeff",
+     streetAddress: "595 sunset rd"
+    };
+
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = { ...employee };
+
+    newObj[key] = value;
+  
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newB = { ...employee };
+    
+    
+
+    delete newB[key];
+    
+    //console.log(newB);
+    return newB;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+
+    return employee;
+}
+
